2e6b7a853d02ee8c86ffcd376268e0feb96cc389
vim235 <vim235@users.noreply.github.com>
2014-08-27 17:01:52 +0900

    Create rename.py
    This is a script in order to rename images.
    The name of directory that has images is input for yourself.

---
+++ rename.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+import os
+import re
+
+TargetPath = '/home/denjo/"name of directory"'
+
+FileList = os.listdir(TargetPath)
+k = len(FileList)
+L = []
+for f in FileList:
+    TS = os.stat(f).st_mtime
+    L.append([TS, f])
+
+L.sort()
+
+p = re.compile(".*jpg$")
+q = re.compile(".*png$")
+r = re.compile(".*gif$")
+i = 1
+j = 0
+while j < k:
+    if p.search("%s" % L[j][1]):
+        os.rename("%s" % L[j][1], "%s.jpg" % str(i))
+        i += 1
+        j += 1
+    elif q.search("%s" % L[j][1]):
+        os.rename("%s" % L[j][1], "%s.png" % str(i))
+        i += 1
+        j += 1
+    elif r.search("%s" % L[j][1]):
+        os.rename("%s" % L[j][1], "%s.gif" % str(i))
+        i += 1
+        j += 1
+    else:
+        j += 1
+

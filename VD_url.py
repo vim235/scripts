bdf94e0c4d93427a89d3143deb655e34603c5b53
ViM_2355 <vim235@users.noreply.github.com>
2014-09-06 18:15:38 +0900

    Update VD_url.py
    This is a script for downloading videos you like.
    Input the urls you want to download.

--- VD_url.py
+++ VD_url.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 #coding: utf-8
 
+# You can download the videos you like from YouTube 
+
 from pytube import YouTube
 from pprint import pprint
 
-url = raw_input('url: ') # input the url you want.
+url = raw_input('url: ') # Input the url you want.
 yt = YouTube()
 yt.url = url 
 pprint(yt.videos)
@@ -12,4 +14,4 @@ print yt.filename
 print yt.filter('mp4')[-1]
 
 video = yt.get('mp4', '360p', 'Baseline')
-video.download('/home/denjo/download')
+video.download('') # Input the name of directory.
